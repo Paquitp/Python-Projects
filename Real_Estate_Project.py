46ce80b31deb9dde63b8eed858d86d19ff4ed952
Paquitp <121645494+Paquitp@users.noreply.github.com>
2023-01-30 19:15:02 -0500

    Add files via upload

---
+++ Real_Estate_Project.py
@@ -0,0 +1,30 @@
+price_house = 1_000_000.0
+
+credit_score = int(input("Your Credit Score: "))
+ 
+if credit_score >= 700:
+  good_credit = credit_score
+  print("Downpayment Interest Rate: 10%")
+  print("Downpayment amount: $" + str(price_house*.10))
+  total_cost= (price_house*.10 + price_house)
+else:
+  print("Downpayment Interest Rate: 20%")
+  print("Downpayment amount: $" + str(price_house*.20))
+  total_cost= (price_house*.20 + price_house)
+
+ 
+
+print("Your total balance + Interest: $" + str(total_cost))
+print("Done")
+
+  
+
+ 
+
+# price = 1_000_000
+# has_good_credit = True
+# if has_good_credit:
+#  down_payment = 0.1 * price
+# else:
+#  down_payment = 0.2 * price
+# print(f"Downpayment: ${down_payment})
---
+++ Weight Converter.py
@@ -0,0 +1,12 @@
+#Weight
+#(K)g or (L)bs:
+#Weight in Kg:
+
+weight = float(input("Your Weight: "))
+unit = str(input("Unit: (K)g or (L)bs"))
+if unit.upper() == "k":  
+ converted = weight / 0.45
+ print("Your weight in Lbs: " + str(converted))
+else: 
+  converted = weight * 0.45
+  print("Your weight in Kg: " + str(converted))
