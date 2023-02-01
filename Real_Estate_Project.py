15a1dc8e0971b59fe53075a5a365471df369c9df
Paquitp <121645494+Paquitp@users.noreply.github.com>
2023-02-01 15:54:55 -0500

    Update Real_Estate_Project.py

--- Real_Estate_Project.py
+++ Real_Estate_Project.py
@@ -4,15 +4,15 @@ credit_score = int(input("Your Credit Score: "))
  
 if credit_score >= 700:
   good_credit = credit_score
-  print("Downpayment Interest Rate: 10%")
+  print("Downpayment Rate: 10%")
   print("Downpayment amount: $" + str(price_house*.10))
   total_cost= (price_house*.10 + price_house)
 else:
-  print("Downpayment Interest Rate: 20%")
+  print("Downpayment Rate: 20%")
   print("Downpayment amount: $" + str(price_house*.20))
   total_cost= (price_house*.20 + price_house)
 
  
 
-print("Your total balance + Interest: $" + str(total_cost))
+print("Your total balance + Downpayment: $" + str(total_cost))
 print("Done")
